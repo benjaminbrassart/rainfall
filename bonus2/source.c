fc40058407cfe22c41f26f4de88f47358bc860a8
Benjamin Brassart <bbrassar@student.42.fr>
2026-05-04 20:01:01 +0200

    Make C source closer to what is really going on when copying localized message

--- bonus2/source.c
+++ bonus2/source.c
@@ -6,20 +6,46 @@ enum language {
 
 static enum language language = LANG_EN;
 
+// "Hyvää päivää "
+static const unsigned char MESSAGE_FI[19] = {
+	0x48, 0x79, 0x76, 0xc3, 0xa4, 0xc3, 0xa4, 0x20, 0x70, 0xc3, 0xa4, 0x69,
+	0x76, 0xc3, 0xa4, 0xc3, 0xa4, 0x20, 0x00
+};
+
+// "Goedemiddag! "
+static const unsigned char MESSAGE_NL[14] = {
+	0x47, 0x6f, 0x65, 0x64, 0x65, 0x6d, 0x69, 0x64, 0x64, 0x61, 0x67, 0x21,
+	0x20, 0x00
+};
+
+// "Hello "
+static const unsigned char MESSAGE_EN[7] = {
+	0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x00
+};
+
 static void greetuser(void)
 {
 	char message[72];
 
 	if (language == LANG_FI) {
-		strncpy(message, "Hyvää päivää ", 19);
+		*(unsigned int *)(message + 0) = *(unsigned int *)(MESSAGE_FI + 0);
+		*(unsigned int *)(message + 4) = *(unsigned int *)(MESSAGE_FI + 4);
+		*(unsigned int *)(message + 8) = *(unsigned int *)(MESSAGE_FI + 8);
+		*(unsigned int *)(message + 12) = *(unsigned int *)(MESSAGE_FI + 12);
+		*(unsigned short *)(message + 16) = *(unsigned short *)(MESSAGE_FI + 16);
+		*(unsigned char *)(message + 18) = *(unsigned char *)(MESSAGE_FI + 18);
 	} else if (language == LANG_NL) {
-		strncpy(message, "Goedemiddag! ", 14);
-		// XXX message._14_2_ = SUB42(uVar1, 2)
+		*(unsigned int *)(message + 0) = *(unsigned int *)(MESSAGE_NL + 0);
+		*(unsigned int *)(message + 4) = *(unsigned int *)(MESSAGE_NL + 4);
+		*(unsigned int *)(message + 8) = *(unsigned int *)(MESSAGE_NL + 8);
+		*(unsigned short *)(message + 12) = *(unsigned short *)(MESSAGE_NL + 12);
 	} else if (language == LANG_EN) {
-		strncpy(message, "Hello ", 7);
+		*(unsigned int *)(message + 0) = *(unsigned int *)(MESSAGE_FI + 0);
+		*(unsigned short *)(message + 4) = *(unsigned short *)(MESSAGE_FI + 4);
+		*(unsigned char *)(message + 6) = *(unsigned char *)(MESSAGE_FI + 6);
 	}
 
-	strcat(message, &stack0x00000004); // ???
+	strcat(message, &stack_buffer_from_main);
 	puts(message);
 }
 
