d29ffe0d75acd95fb4acfcbb9c5c5cac103a9338
Benjamin Brassart <bbrassar@student.42.fr>
2026-05-06 01:09:40 +0200

    Add level3 source

---
+++ level3/source.c
@@ -0,0 +1,19 @@
+int m = 0;
+
+void v(void)
+{
+	char buffer[520];
+
+	fgets(buffer, 512, stdin);
+	printf(buffer);
+
+	if (m == 64) {
+		fwrite("Wait what?!\n", 1, 12, stdout);
+		system("/bin/sh");
+	}
+}
+
+int main(void)
+{
+	v();
+}
